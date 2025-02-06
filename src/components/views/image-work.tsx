447f45a76ea16783e681b103fb8bb47c3094c32d
Kened1221 <106828507+Kened1221@users.noreply.github.com>
2025-02-06 08:44:28 -0500

    imagenes
    tamaño del contenedor de imagenes.

--- src/components/views/image-work.tsx
+++ src/components/views/image-work.tsx
@@ -46,7 +46,7 @@ function ImageWork({ imgs, coordinates }: ImagesContainerProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 h-0">
       {imgs.map((img) => (
         <div
           key={img.id}
